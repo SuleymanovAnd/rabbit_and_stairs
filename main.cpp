61ef5a873dcaa946ae5d91bd9b516fce3fb1c605
Suleymanov Andrey <sasus37@gmail.com>
2022-07-09 12:44:43 +0300

    done

--- main.cpp
+++ main.cpp
@@ -1,6 +1,30 @@
 #include <iostream>
+using namespace std;
 
-int main() {
-    std::cout << "Hello, World!" << std::endl;
-    return 0;
+
+int numberOfWays (int n, int k = 3){
+if (n == 0) {return 1;}
+
+int ways_count = 0;
+
+int jump = 0;
+if (n < k){jump = n;}
+else { jump = k;}
+
+for (int i = 1; i <= jump; i++){
+    ways_count += numberOfWays (n-i);}
+return ways_count;
 }
+int main() {
+    cout << "hello little rabbit, how many stairs do you want to climb?\n";
+    cout << "Enter climb number: ";
+    int n;
+    cin >> n;
+
+   cout <<  numberOfWays (n) << " ways for you!";
+
+
+
+
+}
+
